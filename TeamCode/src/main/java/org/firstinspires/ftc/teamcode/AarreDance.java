44bc057c1e3f81ffda2903c211e779842c3480a2
Aarre Laakso <aarre.laakso@gmail.com>
2018-11-15 16:30:53 -0500

    Add new OpMode to make the robot "dance" (exercise all motors and servos).

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AarreDance.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * This file contains Aarre's experimental code to autonomously "reset" the robot to a state
+ * suitable for running the autonomous game mode
+ *
+ * To avoid issuing an error on the phones, any OpMode class must be
+ * declared public.
+ *
+ */
+
+@SuppressWarnings("WeakerAccess")
+@Autonomous(name="Aarre Autonomous Reset", group="Aarre")
+public class AarreAutonomousReset extends LinearOpMode {
+
+    private AarreRobot robot = new AarreRobot();
+
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Initializing robot");
+        telemetry.update();
+
+        robot.init(hardwareMap, telemetry);
+
+        // Wait for the driver to press PLAY
+        waitForStart();
+
+        robot.raiseArm()
+        robot.lowerArm();
+
+        robot.raiseRiser;
+        robot.lowerRiser();
+
+        robot.raiseHook();
+        robot.lowerHook();
+
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+    }
+
+}
