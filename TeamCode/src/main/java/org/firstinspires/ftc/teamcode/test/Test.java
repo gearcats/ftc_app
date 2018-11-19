b5a0a2cc0701e9c6e5f7342038c33b297c8ac5be
gearcats <7305gearcats@gmail.com>
2018-11-19 12:25:45 -0500

    Version 0.01.001.0003 - Modify gradle configuration for jUnit.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/Test.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.test;
+
+
+/**
+ * Failing test
+ */
+public class Test {
+    @Test
+    public void Test throws Exception
+
+    {
+        assert (false);
+    }
+
+}
